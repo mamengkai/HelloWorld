018c4afdc49159b2bff5a9cd0184e2b46476478d
mamengkai <2050602660@qq.com>
2024-10-30 19:30:22 +0800

    Hello World

---
+++ src/main/java/com/situ/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.situ.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
+
+}
